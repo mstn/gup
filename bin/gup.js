8a3154803a1b9cde30b946b2684dcd12d414aa91
mstn <mstn@git.com>
2015-08-14 18:53:12 +0200

    created load subcommand

--- bin/gup.js
+++ bin/gup.js
@@ -8,23 +8,21 @@ var gup;
 
 program
   .version('0.0.1')
-  .usage('[options] [gtfs zipped file]')
+  .command('load [filepath]')
   .description('upload a dataset to mongodb')
   .option('-t, --tag [value]', 'tag the gtfs dataset')
   .option('-z, --zones [value]', 'use a zone tag')
   .option('-s, --start [value]', 'use a start date')
   .option('-e, --end [value]', 'use a end date')
   .option('-u, --url [value]', 'use a MongoDB url', 'http://localhost:27017/')
-  .option('-d, --db [value]', 'use a MongoDB database', 'test');
-
-program
-  .action(function(filepath){
+  .option('-d, --db [value]', 'use a MongoDB database', 'test')
+  .action(function(filepath, options){
     gup = new Gup({
-      url: program.url + '/' + program.db,
-      tag: program.tag,
-      zones: program.zones,
-      start:moment(program.start).toDate(),
-      end:moment(program.end).toDate()
+      url: options.url + '/' + options.db,
+      tag: options.tag,
+      zones: options.zones,
+      start:moment(options.start).toDate(),
+      end:moment(options.end).toDate()
     });
     gup.load(filepath, function(e){
       if (e){
